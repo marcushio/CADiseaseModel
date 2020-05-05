ae9b9b3672e4c22a5aec946cb3b45d094811e218
marcushio <marcus11tru@gmail.com>
2020-05-05 16:29:24 -0600

    start config file writer

--- src/ConfigFileWriter.java
+++ src/ConfigFileWriter.java
@@ -1,2 +1,24 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
 public class ConfigFileWriter {
+//is this really going to save me time? Maybe I'll make some random map generator
+
+    public static void main(String[] args){
+        String outputFilename = "C:\\Users\\marcu\\OneDrive - University of New Mexico\\CS423 Complex adaptive systems\\Project3\\config.txt";
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(new File(outputFilename), true))){
+            //iterate through 100x100 but maybe I'll make something different later 
+            for(int i = 0; i <= 100; i++){
+                for(int j = 0; j <= 100; j++){
+
+                    if(i < 10 && j < 10 )
+                        writer.write("Node " + i + j);
+                }
+            }
+        }catch(Exception ex){
+            System.out.println("couldn't write results");
+        }
+    }
 }
